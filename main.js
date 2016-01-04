e0c7451136478d91985cf5eb6ffbbd16b775ce57
9000 volts <9000-volts@users.noreply.github.com>
2016-01-04 16:39:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("Hello, world!");
+(function(){
+  var running = false;
+  
+  // Game logic.
+  var g_key = function (e) {
+        console.log("Key Press Detected");
+      },
+      g_activate = function(e) {
+        running = true;
+      }
+  
+  // Code-based activation.
+  var combo_keycodes = [38,38,40,40,37,39,37,39,66,65],
+      combo_index = 0,
+      combo_logic = function (e) {
+        if (e.keyCode === combo_keycodes[index]) index++;
+        else index = 0;
+        if (index == combo_keycodes.length) g_activate();
+      };
+  
+  // Key press behavior.
+  window.onkeydown = function (e) {
+    if (running) g_key(e);
+    else combo_logic(e);
+  };
+})();
